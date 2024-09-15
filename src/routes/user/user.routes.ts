bae456dfdf4fb149ae513c75eb51907de134bf76
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-15 09:38:04 -0400

    feat: route to create and list user

---
+++ src/routes/user/user.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { createUserController } from '../../controllers/user/createUser.controller';
+import { listUserController } from '../../controllers/user/listUser.controller';
+import { ensureAuthMiddleware } from '../../middlewares/ensureAuth.middleware';
+import { validateSerializerMiddleware } from '../../middlewares/validateSerializer.middleware';
+import { userSerializer } from '../../serializers/user.serializers';
+
+export const router = Router();
+
+router.post('', validateSerializerMiddleware(userSerializer), createUserController);
+router.get('', ensureAuthMiddleware, listUserController);
