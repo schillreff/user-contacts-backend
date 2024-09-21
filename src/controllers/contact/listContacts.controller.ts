5fc2b2b6060e1efc1d4ee325656bd550c346c1eb
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-21 10:32:08 -0400

    Revert "feat: list contact controller"
    This reverts commit 2d8cfbc3fdda2905cd3a10cdf7e2242f9b86e96c.

--- src/controllers/contact/listContacts.controller.ts
+++
@@ -1,9 +0,0 @@
-import { instanceToPlain } from 'class-transformer';
-import { Request, Response } from 'express';
-import { listContactsService } from '../../services/contact/listContacts.service';
-
-export const listContactsController = async (req: Request, res: Response) => {
-  const { id } = req.user;
-  const contacts = await listContactsService(id);
-  return res.status(200).json(instanceToPlain(contacts));
-};
