e41ebe44099f75b80cc8875ded6dd04d62e37e55
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-21 10:27:28 -0400

    feat: session route

--- src/routes/session/session.routes.ts
+++ src/routes/session/session.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { createSessionController } from '../../controllers/session/createSession.controller';
 import { validateSerializerMiddleware } from '../../middlewares/validateSerializer.middleware';
-import { sessionUserSerializer } from '../../serializers/user.serializers';
+import { sessionUserSerializer } from '../../serializers/user/user.serializers';
 
 export const router = Router();
 
