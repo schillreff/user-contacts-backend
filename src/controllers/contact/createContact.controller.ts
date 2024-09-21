6f2e6ffde7ee90a8a5de6cf08de95e36fabafcd6
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-21 10:30:13 -0400

    feat: contact controller

---
+++ src/controllers/contact/createContact.controller.ts
@@ -0,0 +1,9 @@
+import { instanceToPlain } from 'class-transformer';
+import { Request, Response } from 'express';
+import { createContactService } from '../../services/contact/createContact.service';
+
+export const createContactController = async (req: Request, res: Response) => {
+  const userID = req.user.id;
+  const createdContact = await createContactService(req.body, userID);
+  return res.status(201).json(instanceToPlain(createdContact));
+};
